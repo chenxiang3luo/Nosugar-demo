eea153d3d2a7da347f7e1293a2ca31222310761e
chenxiang3luo <80572402+chenxiang3luo@users.noreply.github.com>
2023-06-25 12:53:17 +0800

    Update util.py

--- on_off/snippets/util.py
+++ on_off/snippets/util.py
@@ -1,21 +1,13 @@
 import ssl
 import json
 import urllib.request
-host = 'http://mobilelive.market.alicloudapi.com'
-path = '/queryonline'
-method = 'GET'
-number = 15388284660
-appcode = 'e48309910745442f84015d7cfb55ccce'
-querys = f'number={number}'
-bodys = {}
-url = host + path + '?' + querys
-
-request = urllib.request.Request(url)
-request.add_header('Authorization', 'APPCODE ' + appcode)
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
-response = urllib.request.urlopen(request, context=ctx)
-content = response.read()
-print(json.loads(content.decode('utf-8')))
-
+import re
+def validate_phone_number(phone_numbers):
+    for num in phone_numbers:
+        pattern = r'^1[3456789]\d{9}$'
+        match = re.match(pattern, str(num))
+        if match:
+            pass
+        else:
+            return False
+    return True
